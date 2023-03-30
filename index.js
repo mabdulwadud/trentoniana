63511877598304034ec54d881289c549510c6873
spoopitypoop <masumabdulwadud@gmail.com>
2023-03-30 04:08:42 -0400

    updated sources, added info in modals, and wrote alert for mobile devices

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 window.onload=function(){
     var mobile = (/iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));
     if (mobile) {
-        alert("Visit this on a Computer for Better View");              
+        alert("Visit this website on a laptop or desktop computer for the best experience.");              
     } else {
 
     }
