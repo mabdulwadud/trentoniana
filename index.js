f0b5bd9d375d9dffaeea72d0d955f31611bb0590
spoopitypoop <masumabdulwadud@gmail.com>
2023-03-30 03:19:36 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+window.onload=function(){
+    var mobile = (/iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));
+    if (mobile) {
+        alert("Visit this on a Computer for Better View");              
+    } else {
+
+    }
+  }
+
+
+  
