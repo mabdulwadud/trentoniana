4f4ee26cc5467c797176502a4a1a7ea9b3b5b1fa
spoopitypoop <masumabdulwadud@gmail.com>
2023-04-02 03:23:02 -0400

    updated some basic info

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-window.onload=function(){
-    var mobile = (/iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));
-    if (mobile) {
-        alert("Visit this website on a laptop or desktop computer for the best experience.");              
-    } else {
+// window.onload=function(){
+//     var mobile = (/iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));
+//     if (mobile) {
+//         alert("Visit this website on a laptop or desktop computer for the best experience.");              
+//     } else {
 
-    }
-  }
+//     }
+//   }
 
 
   
