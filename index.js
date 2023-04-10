af2505df657a88d4fba7c86691a4baac073de9bd
Masum <masumabdulwadud@gmail.com>
2023-04-10 17:48:19 -0400

    commit

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-window.onload=function(){
-    var mobile = (/iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));
-    if (mobile) {
-        alert("Visit this website on a laptop or desktop computer for the best experience.");              
-    } else {
-
-    }
-  }
+// Alert to mobile users to view on larger display for best experience.
 
 
-  
+// for(var i = 0; i < 1; i++){
+//     window.onload=function(){
+//         var mobile = (/iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));
+//         if (mobile) {
+//             alert("Visit this website on a laptop or desktop computer for the best experience.");              
+//         } else {
+    
+//         }
+//       }
+// }
+
